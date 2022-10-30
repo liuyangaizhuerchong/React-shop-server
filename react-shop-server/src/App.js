4ea48204b206647f2ff6170375040fa9d0732fbf
liuyang <728699244@qq.com>
2022-10-30 20:38:54 +0800

    重新初始化项目-配置antd主题颜色及按需引入

---
+++ react-shop-server/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Button } from "antd";
+import './App.less';
+
+function App() {
+  return (
+    <div>
+      App...
+      <Button type="primary">按钮</Button>
+    </div>
+  );
+}
+
+export default App;
